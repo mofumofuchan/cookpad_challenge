3db168c5c568ed3538603f7585ba1c6bce5bfe5e
mofumofuchan <you_oki1@yahoo.co.jp>
2017-03-16 03:22:29 +0900

    Install devise gem

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'turbolinks', '5.0.1'
 gem 'jbuilder', '2.4.1'
 gem 'bcrypt',   '3.1.11'
 gem 'devise', '4.2.0'
+gem 'carrierwave', '1.0.0'
 
 group :development, :test do
   gem 'sqlite3', '1.3.11'
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170315165216) do
+ActiveRecord::Schema.define(version: 20170315181349) do
 
   create_table "articles", force: :cascade do |t|
     t.text     "content"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "drawing"
     t.index ["user_id", "created_at"], name: "index_articles_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
