bcae2eb514d9e473d4b7cdc5c098e13ea2f4009e
mofumofuchan <you_oki1@yahoo.co.jp>
2017-03-17 01:27:31 +0900

    N+1問題を検出するbullet gemを導入

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,13 @@ Rails.application.configure do
 
   # For "devise" Gem
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # For "bullet" Gem (for N+1 problem)
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get show 1st user" do
-    get '/users/feed/1'
+    get '/users/1'
     assert_response :success
   end
 
