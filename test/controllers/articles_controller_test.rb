3b411cbd6e571d90df820e8dcfc6fd1ef2642232
mofumofuchan <you_oki1@yahoo.co.jp>
2017-03-16 19:41:57 +0900

    modify test

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,15 +1,16 @@
 require 'test_helper'
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+  
+  test "should get new when not signed in" do
     get articles_new_url
-    assert_response :success
+    assert_redirected_to new_user_session_path
   end
 
-  test "should get create" do
-    get articles_create_url
-    assert_response :success
-  end
+  # test "should post create when not signed in" do
+  #   post articles_create_url
+  #   assert_response new_user_session_path
+  # end
 
   test "should get index" do
     get articles_index_url
