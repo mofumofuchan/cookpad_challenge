efb7f3be6922e3dfc7003502a693a0cc8016a40d
mofumofuchan <you_oki1@yahoo.co.jp>
2017-03-18 05:31:58 +0900

    Revert "Userモデルにnicknameとiconカラムを追加"
    This reverts commit faa9ac234279b9fda1f87d6b5f87bd347d377acc.

--- db/migrate/20170315083135_devise_create_users.rb
+++ db/migrate/20170315083135_devise_create_users.rb
@@ -30,13 +30,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.timestamps null: false
 
-      t.string :nickname,           null: false, default: ""
-      t.text   :icon
+      t.timestamps null: false
     end
 
-    add_index :users, :nickname,             unique: true
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
